d4d87d6ab5bc0bdd4801e319fd979cda765232a4
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2016-07-14 14:04:49 -0400

    styling added

---
+++ src/components/search_bar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange(ev) {
+    this.setState({ searchTerm: ev.target.value });
+    this.props.onSearchChange(ev.target.value);
+  }
+  render() {
+    return <input onChange={this.onInputChange} value={this.state.searchTerm} />;
+  }
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar.js';
+import youtubeSearch from './youtube-api.js';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js';
+import debounce from 'lodash.debounce';
 import './style.scss';
 
-$('#main').html('Here we go!');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.search = debounce(this.search, 300);
+    this.search('pixar');
+  }
 
-// following function is based off of that from
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
-// it defines a generator function that returns a continuously incrementing number
-function* countGenerator() {
-  let i = 0;
-  while (i < 10000) {   // if site left open for 10,000 seconds, assume they are not still watching counter
-    yield i++;
+  search(text) {
+    youtubeSearch(text).then(videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar id="searchbar" onSearchChange={(text) => this.search(text)} />
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList videos={this.state.videos} onVideoSelect={selectedVideo => { this.setState({ selectedVideo }); }} />
+        </div>
+      </div>);
   }
 }
+// const App = () => {
+//   return (
+//     <div>
+//       <SearchBar />
+//     </div>);
+// };
 
-const counter = countGenerator();
-
-setInterval(() => {
-  $('#main').html(`You've been on this page for ${counter.next().value} seconds`);
-}, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
